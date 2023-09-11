b1eae7ff9b2495283882cbc683b9ec14869658aa
Akin94 <akin.tutkan@gmail.com>
2023-09-11 16:30:41 +0300

    problem in map

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,32 @@
 import { useQuery, gql } from '@apollo/client';
+import { Console } from 'console';
 
-const GET_LOCATIONS = gql`
-  query GetLocations {
-    locations {
-      id
-      name
-      description
-      photo
-    }
+const COUNTRIES_QUERY = gql`
+query Query {
+  countries {
+    emoji
+    currency
+    name
+    native
+    phone
+    code
   }
+}
 `;
+console.log(COUNTRIES_QUERY)
 function DisplayLocations() {
-  const { loading, error, data } = useQuery(GET_LOCATIONS);
+  const { loading, error, data } = useQuery(COUNTRIES_QUERY);
 
   if (loading) return <p>Loading...</p>;
   if (error) return <p>Error : {error.message}</p>;
 
-  return data.locations.map(({ id, name, description, photo }: any) => (
-    <div key={id}>
+  return data.locations.map(({ item,emoji,currency,name,native,phone,code }: any) => (
+    <div key={item}>
       <h3>{name}</h3>
-      <img width="400" height="250" alt="location-reference" src={`${photo}`} />
+      <img width="400" height="250" alt="location-reference" src={`${emoji}`} />
       <br />
       <b>About this location:</b>
-      <p>{description}</p>
+      <p>{name}</p>
       <br />
     </div>
   ));
